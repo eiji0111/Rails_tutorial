f897f589581ae0359bc4b673f04eb31bca43a413
eiji0111 <ee111fender@gmail.com>
2021-04-14 10:12:50 +0000

    [Update]railsチュートリアル１章

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html:"Hello,world!"
+    render html:"¡hola,mundo!"
+  end
+  
+  def goodbye
+    render html:"goodbye,world!"
   end
   
 end
